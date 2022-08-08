27cbe7b843654203aa80f0143fa67d071cd69215
Chadrac-WAKOMYA <chadracx@gmail.com>
2022-08-08 19:28:25 +0200

    imageProduit model created

---
+++ src/models/imageProduit.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('tb_image_produit', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                isUrl : { msg: 'Utiliser uniquement les URL valides pour les images' },
+                notNull : { msg: '\'image\' est une propriete requise' }
+            }
+        },
+        refProduit: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate:{
+                isInt : { msg: 'Uniquement les nombres entiers sont valides pour refAgence' },
+                notNull : { msg: '\'refProduit\' est une propriete requise' }
+            },
+        },
+    }, {
+        timestamps: true,
+        createdAt: 'created',
+        updatedAt: false
+    })
+  }
