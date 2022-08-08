e2a242b31a47554a80e00c1782f0f13fd50099cf
Chadrac-WAKOMYA <chadracx@gmail.com>
2022-08-08 19:58:25 +0200

    detailCommande model created

---
+++ src/models/detailCommande.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('tb_categorie_produit', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        qteCommande: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate:{
+                isInt : { msg: 'Uniquement les nombres entiers sont valides pour qteStock' },
+                notNull : { msg: '\'qteCommande\' est une propriete requise' }
+            },
+        },
+        montant: {
+            type: DataTypes.DOUBLE,
+            allowNull: false,
+            validate:{
+                notEmpty : { msg: '\'montant\' ne doit pas être vide' },
+                notNull : { msg: '\'montant\' est une propriété requise' }
+            }
+        },
+        refCommande: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate:{
+                isInt : { msg: 'Uniquement les nombres entiers sont valides pour refAgence' },
+                notNull : { msg: '\'refCommande\' est une propriete requise' }
+            },
+        },
+        refProduit: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate:{
+                isInt : { msg: 'Uniquement les nombres entiers sont valides pour refCategorie' },
+                notNull : { msg: '\'refProduit\' est une propriete requise' }
+            },
+        }
+    }, {
+        timestamps: true,
+        createdAt: 'created',
+        updatedAt: false
+    })
+  }
