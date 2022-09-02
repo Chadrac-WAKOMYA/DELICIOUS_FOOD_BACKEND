ac2602e1146f31bbbcdb01a134aa3cfe722c60e4
Akim <gentilakili98@gmail.com>
2022-09-02 05:29:17 +0200

    Add cors

--- app.js
+++ app.js
@@ -1,89 +1,104 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const sequelize = require('./src/db/sequelize')
+const express = require("express");
+const bodyParser = require("body-parser");
+const sequelize = require("./src/db/sequelize");
 
-const app = express()
-const port = process.env.PORT || 3500
+const app = express();
+const port = process.env.PORT || 3500;
+const cors = require("cors");
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+sequelize.initDb();
 
-sequelize.initDb()
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello Heroku !" });
+});
 
-app.get('/', (req, res) => {
-    res.json({ msg: 'Hello Heroku !'})
-})
-    
 // Ici nos futurs point de terminaison
-require('./src/routes/client/createClient')(app)
-require('./src/routes/client/findAllClients')(app)
-require('./src/routes/client/updateClient')(app)
-require('./src/routes/client/findClientByPK')(app)
-require('./src/routes/client/deleteClient')(app)
+require("./src/routes/client/createClient")(app);
+require("./src/routes/client/findAllClients")(app);
+require("./src/routes/client/updateClient")(app);
+require("./src/routes/client/findClientByPK")(app);
+require("./src/routes/client/deleteClient")(app);
 
-require('./src/routes/bareme/createBareme')(app)
-require('./src/routes/bareme/findAllBaremes')(app)
-require('./src/routes/bareme/findAllBaremeByPK')(app)
-require('./src/routes/bareme/updateBareme')(app)
-require('./src/routes/bareme/deleteBareme')(app)
+require("./src/routes/bareme/createBareme")(app);
+require("./src/routes/bareme/findAllBaremes")(app);
+require("./src/routes/bareme/findAllBaremeByPK")(app);
+require("./src/routes/bareme/updateBareme")(app);
+require("./src/routes/bareme/deleteBareme")(app);
 
-require('./src/routes/commande/createCommande')(app)
-require('./src/routes/commande/findAllCommandes')(app)
-require('./src/routes/commande/findAllCommandeByPK')(app)
-require('./src/routes/commande/updateCommande')(app)
-require('./src/routes/commande/deleteCommande')(app)
+require("./src/routes/commande/createCommande")(app);
+require("./src/routes/commande/findAllCommandes")(app);
+require("./src/routes/commande/findAllCommandeByPK")(app);
+require("./src/routes/commande/updateCommande")(app);
+require("./src/routes/commande/deleteCommande")(app);
 
-require('./src/routes/agence/createAgence')(app)
-require('./src/routes/agence/findAllAgences')(app)
-require('./src/routes/agence/findAllAgencesByPK')(app)
-require('./src/routes/agence/deleteAgence')(app)
-require('./src/routes/agence/updateAgence')(app)
+require("./src/routes/agence/createAgence")(app);
+require("./src/routes/agence/findAllAgences")(app);
+require("./src/routes/agence/findAllAgencesByPK")(app);
+require("./src/routes/agence/deleteAgence")(app);
+require("./src/routes/agence/updateAgence")(app);
 
-require('./src/routes/categorieProduit/createCategorieProduit')(app)
-require('./src/routes/categorieProduit/findAllCategorieProduits')(app)
-require('./src/routes/categorieProduit/findAllCategorieProduitByPK')(app)
-require('./src/routes/categorieProduit/updateCategorieProduit')(app)
-require('./src/routes/categorieProduit/deleteCategorieProduit')(app)
+require("./src/routes/categorieProduit/createCategorieProduit")(app);
+require("./src/routes/categorieProduit/findAllCategorieProduits")(app);
+require("./src/routes/categorieProduit/findAllCategorieProduitByPK")(app);
+require("./src/routes/categorieProduit/updateCategorieProduit")(app);
+require("./src/routes/categorieProduit/deleteCategorieProduit")(app);
 
-require('./src/routes/produit/createProduit')(app)
-require('./src/routes/produit/findAllProduits')(app)
-require('./src/routes/produit/findAllProduitByPK')(app)
-require('./src/routes/produit/updateProduit')(app)
-require('./src/routes/produit/deleteProduit')(app)
+require("./src/routes/produit/createProduit")(app);
+require("./src/routes/produit/findAllProduits")(app);
+require("./src/routes/produit/findAllProduitByPK")(app);
+require("./src/routes/produit/updateProduit")(app);
+require("./src/routes/produit/deleteProduit")(app);
 
-require('./src/routes/imageProduit/createImageProduit')(app)
-require('./src/routes/imageProduit/findAllImageProduits')(app)
-require('./src/routes/imageProduit/findAllImageProduitByPK')(app)
-require('./src/routes/imageProduit/updateImageProduit')(app)
-require('./src/routes/imageProduit/deleteImageProduit')(app)
+require("./src/routes/imageProduit/createImageProduit")(app);
+require("./src/routes/imageProduit/findAllImageProduits")(app);
+require("./src/routes/imageProduit/findAllImageProduitByPK")(app);
+require("./src/routes/imageProduit/updateImageProduit")(app);
+require("./src/routes/imageProduit/deleteImageProduit")(app);
 
-require('./src/routes/detailCommande/createDetailCommande')(app)
-require('./src/routes/detailCommande/findAllDetailCommandes')(app)
-require('./src/routes/detailCommande/findAllDetailCommandeByPK')(app)
-require('./src/routes/detailCommande/updateDetailCommande')(app)
-require('./src/routes/detailCommande/deleteDetailCommande')(app)
+require("./src/routes/detailCommande/createDetailCommande")(app);
+require("./src/routes/detailCommande/findAllDetailCommandes")(app);
+require("./src/routes/detailCommande/findAllDetailCommandeByPK")(app);
+require("./src/routes/detailCommande/updateDetailCommande")(app);
+require("./src/routes/detailCommande/deleteDetailCommande")(app);
 
+require("./src/routes/notationProduit/createNotationProduit")(app);
+require("./src/routes/notationProduit/findAllNotationProduits")(app);
+require("./src/routes/notationProduit/findAllNotationProduitByPK")(app);
+require("./src/routes/notationProduit/updateNotationProduit")(app);
+require("./src/routes/notationProduit/deleteNotationProduit")(app);
 
-require('./src/routes/notationProduit/createNotationProduit')(app)
-require('./src/routes/notationProduit/findAllNotationProduits')(app)
-require('./src/routes/notationProduit/findAllNotationProduitByPK')(app)
-require('./src/routes/notationProduit/updateNotationProduit')(app)
-require('./src/routes/notationProduit/deleteNotationProduit')(app)
+require("./src/routes/statsVisualize/createStatsVisualize")(app);
+require("./src/routes/statsVisualize/findAllStatsVisualizes")(app);
+require("./src/routes/statsVisualize/findAllStatsVisualizeByPK")(app);
+require("./src/routes/statsVisualize/updateStatsVisualize")(app);
+require("./src/routes/statsVisualize/deleteStatsVisualize")(app);
 
+require("./src/routes/livreur/createLivreur")(app);
+require("./src/routes/livreur/findAllLivreurs")(app);
+require("./src/routes/livreur/findAllLivreurByPK")(app);
+require("./src/routes/livreur/updateLivreur")(app);
+require("./src/routes/livreur/deleteLivreur")(app);
 
-require('./src/routes/statsVisualize/createStatsVisualize')(app)
-require('./src/routes/statsVisualize/findAllStatsVisualizes')(app)
-require('./src/routes/statsVisualize/findAllStatsVisualizeByPK')(app)
-require('./src/routes/statsVisualize/updateStatsVisualize')(app)
-require('./src/routes/statsVisualize/deleteStatsVisualize')(app)
+app.get("/delicious_food_api/home", (req, res) => {
+  res.json({ msg: "server runs well" });
+});
 
-require('./src/routes/livreur/createLivreur')(app)
-require('./src/routes/livreur/findAllLivreurs')(app)
-require('./src/routes/livreur/findAllLivreurByPK')(app)
-require('./src/routes/livreur/updateLivreur')(app)
-require('./src/routes/livreur/deleteLivreur')(app)
-
-
-app.get('/delicious_food_api/home', (req, res) => {
-    res.json({ msg: 'server runs well'})
-})
-
-app.listen(port,()=>console.log(`Notre application Node est demaree sur : http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(
+    `Notre application Node est demaree sur : http://localhost:${port}`
+  )
+);
